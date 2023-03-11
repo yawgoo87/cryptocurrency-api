9436ddeff1a90e7fa65b57a3862e6159f5c6cd93
yawgoo87 <yawgoo87@proton.me>
2023-03-11 17:15:55 +0200

    Fixing [/amountBurned] route

--- routes/amountBurned.js
+++ routes/amountBurned.js
@@ -14,6 +14,7 @@ router.get("/", async (req, res, next) => {
   const decimals = await tokenContract.methods.decimals().call();
   
   txnData = await web3.eth.getTransactionReceipt(deploymentTxn);
+  console.log(txnData)
   tokenDeploymentBlock = await txnData.blockNumber;
   currentBlock = await web3.eth.getBlockNumber();
 
